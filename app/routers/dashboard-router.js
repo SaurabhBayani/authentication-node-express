502dd12831c19234d61f7ad66d6c538e2c41ecc8
Saurabh Bayani <saurabh.bayani@gmail.com>
2018-01-20 21:00:00 +0530

    Adding validation for dashboard page - User should be logged in

--- app/routers/dashboard-router.js
+++ app/routers/dashboard-router.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
-router.get('/', function(req, res) {
-    res.render('dashboard.ejs');
+function checkUserSignedIn(req, res, next) {
+    if(req.session.user){
+        next();
+    } else {
+        var error = new Error('Not logged in');
+        next(error);
+    }
+}
+
+router.get('/', checkUserSignedIn, function(req, res, next, err) {
+    res.render('dashboard.ejs', {userName: req.session.user.userName});
 });
+
 module.exports = router;
